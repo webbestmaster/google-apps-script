2ce53e0b18f00a5da82ff02cdb6233d23b0d556c
Dmitry Turovtsov <dmitry.turovtsov@consultant.volvo.com>
2022-11-11 10:30:38 +0100

    refactor

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Google apps script - example
---
+++ src/code.ts
@@ -0,0 +1,23 @@
+/* global Logger, SpreadsheetApp, GoogleAppsScript */
+function greeter(person: string): string {
+    return `Hello, ${person}!`;
+}
+
+const user = 'Grant';
+
+Logger.log(greeter(user));
+
+const appUI = SpreadsheetApp.getUi();
+const response: GoogleAppsScript.Base.PromptResponse = appUI.prompt(
+    'Getting to know you',
+    'May I know your name?',
+    appUI.ButtonSet.YES_NO
+);
+
+if (response.getSelectedButton() === appUI.Button.YES) {
+    Logger.log("The user's name is %s.", response.getResponseText());
+} else if (response.getSelectedButton() === appUI.Button.NO) {
+    Logger.log("The user didn't want to provide a name.");
+} else {
+    Logger.log("The user clicked the close button in the dialog's title bar.");
+}
